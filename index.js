d096bdf23eafebc521df83f0b822e7a55675d0e6
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-02-23 02:39:09 -0500

    Sidebar mobile updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // c-nav--toggled
 var nav = document.getElementById("nav");
+var logo = document.getElementById("logo");
 
 if (window.innerWidth <= 600) {
     onToggle();
@@ -7,4 +8,10 @@ if (window.innerWidth <= 600) {
 
 function onToggle() {
     nav.classList.toggle("c-nav--toggled");
+
+    if (nav.classList.contains("c-nav--toggled")) {
+        logo.innerText = "A";
+    } else {
+        logo.innerText = "Avocado";
+    }
 }
